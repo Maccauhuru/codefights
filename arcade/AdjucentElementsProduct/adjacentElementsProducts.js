5b3b878db6d8a7dba3b1059c2d41502dc81b8d80
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-04-29 19:38:10 -0500

    refactored solution

--- arcade/AdjucentElementsProduct/adjacentElementsProducts.js
+++ arcade/AdjucentElementsProduct/adjacentElementsProducts.js
@@ -1,10 +1,13 @@
-const adjacentElementsProduct = (inputArray) => {
+const adjacentElementsProduct = inputArray => {
   let minProductSum = 0;
-  let maxProductSum = inputArray[0] * inputArray[1];//Give mas initial value
+  let maxProductSum = inputArray[0] * inputArray[1]; //Give max an initial value
 
   for (let i = 1; i < inputArray.length - 1; i++) {
-    minProductSum = inputArray[i] * inputArray[i+1]; // Give min a value 
-    if (minProductSum > maxProductSum) {  // Compare if new min > current max and replace 
+    // Give min a value
+    minProductSum = inputArray[i] * inputArray[i + 1];
+
+    if (minProductSum > maxProductSum) {
+      // Compare if new min > current max and replace
       maxProductSum = minProductSum;
     }
   }
